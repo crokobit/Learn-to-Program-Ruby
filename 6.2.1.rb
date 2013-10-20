2a2027b463a1610396c64b19a25e2ad7c139e926
crokobit <crokobit@gmail.com>
2013-10-20 10:09:12 +0800

    add 6.2.1.rb

---
+++ 6.2.1.rb
@@ -0,0 +1,6 @@
+#WHADDAYA MEAN "I WANT A RAISE"?!? YOU'RE FIRED!!
+puts 'type what you want to say to boss'
+puts 'type exit if you want to leave'
+while ( input = gets.chomp ) != "exit"
+	puts 'WHADDAYA MEAN "' + input + '"?!? YOU\'RE FIRED!!'
+end
